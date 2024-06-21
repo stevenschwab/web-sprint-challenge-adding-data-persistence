8c691fbfbee129946ed24208b2ee6ec4291d66a5
Steve Schwab <schwabs@umich.edu>
2024-06-20 17:20:58 -0700

    create tasks table

--- data/migrations/20240621000047_first_migration.js
+++ data/migrations/20240621000047_first_migration.js
@@ -12,6 +12,7 @@ exports.up = async function(knex) {
     table.integer('project_completed')
         .unsigned()
         .defaultTo(0)
+        .notNullable()
   })
   .createTable('resources', table => {
     table.increments('resource_id')
@@ -19,6 +20,22 @@ exports.up = async function(knex) {
         .unique()
         .notNullable()
     table.string('resource_description')
+  })
+  .createTable('tasks', table => {
+    table.increments('task_id')
+    table.string('task_description')
+        .notNullable()
+    table.string('task_notes')
+    table.integer('task_completed')
+        .unsigned()
+        .defaultTo(0)
+        .notNullable()
+    table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
   })
 };
 
