206fa6372118f5c822345945aba2bc1d3d151e7d
Steve Schwab <schwabs@umich.edu>
2024-06-20 17:58:30 -0700

    create dummy data for projects and resources tables

---
+++ data/seeds/02-make-projects.js
@@ -0,0 +1,16 @@
+const projects = [
+    { project_name: "Project 1", project_description: "" },
+    { project_name: "Project 2", project_description: "" },
+    { project_name: "Project 3", project_description: "" },
+]
+
+const resources = [
+    { resource_name: "", resource_description: "" },
+    { resource_name: "", resource_description: "" },
+    { resource_name: "", resource_description: "" },
+]
+
+
+exports.seed = function (knex) {
+
+}
