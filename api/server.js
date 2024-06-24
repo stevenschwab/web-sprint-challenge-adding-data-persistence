82273e8f2d6da2bc69b98dfa1bb44afd2d9c6118
Steve Schwab <schwabs@umich.edu>
2024-06-24 16:00:14 -0700

    create server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // build your server here and require it from index.js
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('*', (req, res, next) => { //eslint-disable-line
+    res.json({ message: "API is up" })
+})
+
+module.exports = server
