5921413ececc2cbc6073d5d42cc1c056a095ec02
Steve Schwab <schwabs@umich.edu>
2024-06-25 13:16:06 -0700

    import tasks router to the server and use it on the route /api/tasks

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const ResourcesRouter = require('./resource/router')
 const ProjectsRouter = require('./project/router')
+const TasksRouter = require('./task/router')
 
 const server = express()
 
@@ -8,6 +9,7 @@ server.use(express.json())
 
 server.use('/api/resources', ResourcesRouter)
 server.use('/api/projects', ProjectsRouter)
+server.use('/api/tasks', TasksRouter)
 
 server.use('*', (req, res, next) => { //eslint-disable-line
     res.json({ message: "API is up" })
