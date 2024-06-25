5406e2d54fe6abcc2ec118e7f21e89e0daf0c7da
Steve Schwab <schwabs@umich.edu>
2024-06-25 13:19:28 -0700

    create skeleton template of endpoints in tasks router

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,21 @@
-// build your `/api/tasks` router here
+const express = require('express')
+const router = express.Router()
+const TasksModel = require('./model')
+
+router.post('/', (req, res, next) => {
+
+})
+
+router.get('/', (req, res, next) => {
+
+})
+
+router.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        errorMessage: error.message,
+        message: 'something bad happened in the task-router',
+        stack: error.stack
+    })
+})
+
+module.exports = router
